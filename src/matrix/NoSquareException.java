d3cbb5d52463fcbfad2f70caef20ac5b88d10199
Venkat Arun <venkat@mpi-sws.org>
2016-07-12 18:09:52 +0200

    Matrix library for computing inverse

---
+++ src/matrix/NoSquareException.java
@@ -0,0 +1,15 @@
+package matrix;
+
+public class NoSquareException extends Exception {
+
+	public NoSquareException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSquareException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
