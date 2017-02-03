9edd1968ba196f95af57ceb4ae3390e8535c56e7
Venkat Arun <venkatarun95@gmail.com>
2017-02-03 13:18:35 +0530

    Point generation by truncating after shifting

--- src/matching/generate-pts.cc
+++ src/matching/generate-pts.cc
@@ -1,5 +1,9 @@
 #include <cassert>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <random>
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -20,17 +24,20 @@ struct VectorHash {
 typedef vector<double> Vector;
 typedef unordered_set< Vector, VectorHash > VectorSet;
 
-int round(double x) {
-	if (x >= 0)
-		return (int) (x + 0.5);
-	return (int) (x - 0.5);
-}
+default_random_engine rand_gen;
+uniform_real_distribution<double> uniform_rand(0.0,1.0);
 
-double abs(double x) {
-	if (x > 0)
-		return x;
-	return -x;
-}
+// int round(double x) {
+// 	if (x >= 0)
+// 		return (int) (x + 0.5);
+// 	return (int) (x - 0.5);
+// }
+
+// double abs(double x) {
+// 	if (x > 0)
+// 		return x;
+// 	return -x;
+// }
 
 double dist(const Vector& a, const Vector& b) {
   if (a.size() != b.size())
@@ -45,6 +52,40 @@ void print_vec(const Vector& vec) {
   for (const auto& x : vec)
     cout << x << " ";
   cout << endl;
+}
+
+bool intersect(const VectorSet& a, const VectorSet& b) {
+	for (const auto& x : a)
+		if (b.count(x))
+			return true;
+}
+
+// Return a random vector in [0, 1]^d.
+Vector rand_vector(int d) {
+	Vector res(d, 0);
+	for (int i = 0; i < d; ++i)
+		res[i] = uniform_rand(rand_gen);
+	return res;
+}
+
+// Find norm of a vector.
+double norm(const Vector& vec, int l=2) {
+	assert(l == 2);
+	double res = 0;
+	for (const auto& x : vec)
+		res += x * x;
+	return sqrt(res);
+}
+
+void scalar_mult(Vector& vec, double factor) {
+	for (int i = 0; i < vec.size(); ++i)
+		vec[i] *= factor;
+}
+
+// Do a = a + b.
+void add_vec(Vector& a, const Vector& b) {
+	for (int i = 0; i < a.size(); ++i)
+		a[i] += b[i];
 }
 
 void gen_pts_recur(const Vector& center, Vector& cur, const double radius, VectorSet& visited) {
@@ -68,7 +109,7 @@ void gen_pts_recur(const Vector& center, Vector& cur, const double radius, Vecto
   // Find the odd index
   int odd = -1; // Index which is odd
   for (int i = 0; i < cur.size(); ++i) {
-    if (round(cur[i]) % 2) {
+    if ((int)round(cur[i]) % 2) {
       assert(odd == -1);
       odd = i;
     }
@@ -105,13 +146,12 @@ void gen_pts_recur(const Vector& center, Vector& cur, const double radius, Vecto
   cur[odd] += 1;
 }
 
-int gen_pts(const Vector& center, const double radius) {
+int gen_pts(const Vector& center, const double radius, VectorSet& res) {
   Vector cur(center);
-	VectorSet res;
 
   // Convert to one-odd
   for (int i = 0; i < cur.size(); ++i) {
-    if (round(cur[i]) % 2)
+    if ((int)round(cur[i]) % 2)
       cur[i] = (abs(round(cur[i])+1 - cur[i]) <= 1) ? round(cur[i])+1 : round(cur[i])-1;
 		else
 			cur[i] = round(cur[i]);
@@ -126,23 +166,57 @@ int gen_pts(const Vector& center, const double radius) {
 	return res.size();
 }
 
-bool intersect(const VectorSet& a, const VectorSet& b) {
-	for (const auto& x : a)
-		if (b.count(x))
-			return true;
+int gen_pts_d(const Vector& center, const double radius, VectorSet& res) {
+	Vector cur(center);
+	for (int i = 0; i < center.size(); ++i) {
+		for (int j = 0; j < cur.size(); ++j)
+			cur[j] += 1.0 + 1.0/center.size();
+		Vector next(cur);
+		for (int j = 0; j < cur.size(); ++j)
+			next[j] = int(cur[j] / radius) * radius;
+		res.insert(next);
+	}
+	return res.size();
 }
 
 int main() {
+	rand_gen.seed(chrono::system_clock::now().time_since_epoch().count());
+	
+	// Individual Tests
+	
 	// int dim = 5;
-	// double radius = 4; //1 * dim;
+	// double radius = 1.0 / 4.0; //1 * dim;
 	// Vector c1(dim, 0.9);
-	// cout << gen_pts(c1, radius) << endl;
+	// VectorSet res;
+	// cout << setprecision(4) << gen_pts_d(c1, radius, res) << endl;
+	// for (const auto& x : res)
+	// 	print_vec(x);
 
-	for (int dim = 2; dim < 100; ++dim) {
-		Vector c1(dim, 2);
-		//print_vec(c1);
-		double radius = 2;
-		cout << dim << " " << radius << " " << gen_pts(c1, radius) << endl;
-	}
+	
+	// Test growth with number of dimensions
+	
+	// for (int dim = 2; dim < 100; ++dim) {
+	// 	Vector c1(dim, 2);
+	// 	VectorSet res;
+	// 	//print_vec(c1);
+	// 	double radius = 1.0 / 64;
+	// 	cout << dim << " " << radius << " " << gen_pts_d(c1, radius, res) << endl;
+	// }
+
+
+	// Test accuracy
+
+	int dim = 100;
+	double radius = 1.0 / 64.0;
+	Vector a = rand_vector(dim);
+	Vector b = rand_vector(dim);
+	scalar_mult(b, 0.25 * radius / norm(b));
+	cout << norm(b) << " " << norm(a) << endl;
+	add_vec(b, a);
+
+	VectorSet apts, bpts;
+	gen_pts_d(a, radius, apts);
+	gen_pts_d(b, radius, bpts);
+	cout << norm(b) << " " << intersect(apts, bpts) << endl;
   return 0;
 }
